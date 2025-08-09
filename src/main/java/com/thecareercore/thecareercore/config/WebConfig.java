a1c3f4dfb40a8087d713480d57b41fdf5e18e420
mfonmfon <mfonm579@gmail.com>
2025-08-09 10:24:37 +0100

    updated

--- src/main/java/com/thecareercore/thecareercore/config/WebConfig.java
+++ src/main/java/com/thecareercore/thecareercore/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig  implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl)
+                .allowedOrigins("https://thecareercore.africa", "https://www.thecareercore.africa")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
